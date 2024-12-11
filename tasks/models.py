a004e028789f9c8276f1e5ec46c55c4fbc1707b2
Karel <karelh2207@gmail.com>
2024-12-11 10:23:55 -0300

    updating

--- tasks/models.py
+++ tasks/models.py
@@ -4,5 +4,4 @@ from django.db import models
 class Task(models.Model):
   title = models.CharField(max_length=100)
   description = models.TextField(blank=True)
-  completed = models.BooleanField(default=False)
   created_at = models.DateTimeField(auto_now_add=True)
